c35e7b126311603881fbf4d766a0bf85bc42e814
Antonin RAFFIN <antonin.raffin@ensta.org>
2018-12-27 10:23:59 +0100

    Add tensorboard support

--- algos/ddpg_with_vae.py
+++ algos/ddpg_with_vae.py
@@ -1,11 +1,5 @@
-# Copyright (c) 2018 Roma Sokolkov
-# MIT License
-
-"""
-DDPGWithVAE inherits DDPG from stable-baselines
-and reimplements learning method.
-"""
-
+# Original author: Roma Sokolkov
+# Edited by Antonin Raffin
 import time
 
 import numpy as np
@@ -13,124 +7,128 @@ from mpi4py import MPI
 
 from stable_baselines import logger
 from stable_baselines.ddpg.ddpg import DDPG
+from stable_baselines.common import TensorboardWriter
 
 
 class DDPGWithVAE(DDPG):
     """
-    Modified learn method from stable-baselines
+    Custom DDPG version in order to work with donkey car env.
+    It is adapted from the stable-baselines version.
 
-    - Stop rollout on episode done.
-    - More verbosity.
+    Changes:
+    - optimization is done after each episode
+    - more verbosity.
     """
     def learn(self, total_timesteps, callback=None, seed=None,
-              log_interval=1, print_freq=100):
-        rank = MPI.COMM_WORLD.Get_rank()
-        # we assume symmetric actions.
-        assert np.all(np.abs(self.env.action_space.low) == self.env.action_space.high)
+              log_interval=1, tb_log_name="DDPG", print_freq=100):
+        with TensorboardWriter(self.graph, self.tensorboard_log, tb_log_name) as writer:
 
-        self.episode_reward = np.zeros((1,))
-        with self.sess.as_default(), self.graph.as_default():
-            # Prepare everything.
-            self._reset()
-            episode_reward = 0.
-            episode_step = 0
-            episodes = 0
-            step = 0
-            total_steps = 0
+            rank = MPI.COMM_WORLD.Get_rank()
+            # we assume symmetric actions.
+            assert np.all(np.abs(self.env.action_space.low) == self.env.action_space.high)
 
-            start_time = time.time()
+            self.episode_reward = np.zeros((1,))
+            with self.sess.as_default(), self.graph.as_default():
+                # Prepare everything.
+                self._reset()
+                episode_reward = 0.
+                episode_step = 0
+                episodes = 0
+                step = 0
+                total_steps = 0
 
-            actor_losses = []
-            critic_losses = []
-            should_return = False
+                start_time = time.time()
 
-            while True:
-                obs = self.env.reset()
-                # Rollout one episode.
-                while True:
-                    if total_steps >= total_timesteps:
-                        if should_return:
-                            return self
-                        should_return = True
-                        break
-
-                    # Predict next action.
-                    action, q_value = self._policy(obs, apply_noise=True, compute_q=True)
-                    if self.verbose >= 2:
-                        print(action)
-                    assert action.shape == self.env.action_space.shape
-
-                    # Execute next action.
-                    new_obs, reward, done, info = self.env.step(action * np.abs(self.action_space.low))
-
-                    if info.get('error_too_high'):
-                        time.sleep(1)
-                        self._reset()
-                        obs = self.env.reset()
-
-                    step += 1
-                    total_steps += 1
-                    if rank == 0 and self.render:
-                        self.env.render()
-                    episode_reward += reward
-                    episode_step += 1
-
-                    # Book-keeping.
-                    self._store_transition(obs, action, reward, new_obs, done)
-
-                    obs = new_obs
-                    if callback is not None:
-                        callback(locals(), globals())
-
-                    if done:
-                        print("Episode finished. Reward: {:.2f}".format(episode_reward))
-                        # Episode done.
-                        episode_reward = 0.
-                        episode_step = 0
-                        episodes += 1
-
-                        self._reset()
-                        obs = self.env.reset()
-                        # Finish rollout on episode finish.
-                        break
-                        
-                # Train VAE.
-                train_start = time.time()
-
-                # Train DDPG.
                 actor_losses = []
                 critic_losses = []
-                train_start = time.time()
-                for t_train in range(self.nb_train_steps):
-                    critic_loss, actor_loss = self._train_step(0, None, log=t_train == 0)
-                    critic_losses.append(critic_loss)
-                    actor_losses.append(actor_loss)
-                    self._update_target_net()
-                print("DDPG training duration: {:.2f}".format(time.time() - train_start))
+                should_return = False
 
-                mpi_size = MPI.COMM_WORLD.Get_size()
-                # Log stats.
-                # XXX shouldn't call np.mean on variable length lists
-                duration = time.time() - start_time
-                stats = self._get_stats()
-                combined_stats = stats.copy()
-                combined_stats['train/loss_actor'] = np.mean(actor_losses)
-                combined_stats['train/loss_critic'] = np.mean(critic_losses)
-                combined_stats['total/duration'] = duration
-                combined_stats['total/steps_per_second'] = float(step) / float(duration)
-                combined_stats['total/episodes'] = episodes
+                while True:
+                    obs = self.env.reset()
+                    # Rollout one episode.
+                    while True:
+                        if total_steps >= total_timesteps:
+                            if should_return:
+                                return self
+                            should_return = True
+                            break
 
-                combined_stats_sums = MPI.COMM_WORLD.allreduce(
-                    np.array([as_scalar(x) for x in combined_stats.values()]))
-                combined_stats = {k: v / mpi_size for (k, v) in zip(combined_stats.keys(), combined_stats_sums)}
+                        # Predict next action.
+                        action, q_value = self._policy(obs, apply_noise=True, compute_q=True)
+                        if self.verbose >= 2:
+                            print(action)
+                        assert action.shape == self.env.action_space.shape
 
-                # Total statistics.
-                combined_stats['total/steps'] = step
+                        # Execute next action.
+                        new_obs, reward, done, info = self.env.step(action * np.abs(self.action_space.low))
 
-                for key in sorted(combined_stats.keys()):
-                    logger.record_tabular(key, combined_stats[key])
-                logger.dump_tabular()
-                logger.info('')
+                        step += 1
+                        total_steps += 1
+                        if rank == 0 and self.render:
+                            self.env.render()
+                        episode_reward += reward
+                        episode_step += 1
+
+                        if print_freq > 0 and episode_step % print_freq == 0 and episode_step > 0:
+                            print("{} steps".format(episode_step))
+
+                        # Book-keeping.
+                        self._store_transition(obs, action, reward, new_obs, done)
+
+                        obs = new_obs
+                        if callback is not None:
+                            # Only stop training if return value is False, not when it is None. This is for backwards
+                            # compatibility with callbacks that have no return statement.
+                            if callback(locals(), globals()) is False:
+                                return self
+
+                        if done:
+                            print("Episode finished. Reward: {:.2f} {} Steps".format(episode_reward, episode_step))
+
+                            # Episode done.
+                            episode_reward = 0.
+                            episode_step = 0
+                            episodes += 1
+
+                            self._reset()
+                            obs = self.env.reset()
+                            # Finish rollout on episode finish.
+                            break
+
+                    # Train DDPG.
+                    actor_losses = []
+                    critic_losses = []
+                    train_start = time.time()
+                    for t_train in range(self.nb_train_steps):
+                        critic_loss, actor_loss = self._train_step(0, None, log=t_train == 0)
+                        critic_losses.append(critic_loss)
+                        actor_losses.append(actor_loss)
+                        self._update_target_net()
+                    print("DDPG training duration: {:.2f}".format(time.time() - train_start))
+
+                    mpi_size = MPI.COMM_WORLD.Get_size()
+                    # Log stats.
+                    # XXX shouldn't call np.mean on variable length lists
+                    duration = time.time() - start_time
+                    stats = self._get_stats()
+                    combined_stats = stats.copy()
+                    combined_stats['train/loss_actor'] = np.mean(actor_losses)
+                    combined_stats['train/loss_critic'] = np.mean(critic_losses)
+                    combined_stats['total/duration'] = duration
+                    combined_stats['total/steps_per_second'] = float(step) / float(duration)
+                    combined_stats['total/episodes'] = episodes
+
+                    combined_stats_sums = MPI.COMM_WORLD.allreduce(
+                        np.array([as_scalar(x) for x in combined_stats.values()]))
+                    combined_stats = {k: v / mpi_size for (k, v) in zip(combined_stats.keys(), combined_stats_sums)}
+
+                    # Total statistics.
+                    combined_stats['total/steps'] = step
+
+                    for key in sorted(combined_stats.keys()):
+                        logger.record_tabular(key, combined_stats[key])
+                    logger.dump_tabular()
+                    logger.info('')
 
 
 def as_scalar(scalar):
--- algos/sac_vae.py
+++ algos/sac_vae.py
@@ -5,11 +5,30 @@ import numpy as np
 from stable_baselines import SAC
 from stable_baselines import logger
 from stable_baselines.common.vec_env import VecEnv
+from stable_baselines.a2c.utils import total_episode_reward_logger
 from stable_baselines.ppo2.ppo2 import safe_mean, get_schedule_fn
+from stable_baselines.common import TensorboardWriter
 
 
 class SACWithVAE(SAC):
+    """
+    Custom version of Soft Actor-Critic (SAC) to use it with donkey car env.
+    It is adapted from the stable-baselines version.
+
+    Notable changes:
+    - optimization is done after each episode and not at every step
+    - this version is integrated with teleoperation
+
+    """
     def optimize(self, step, writer, current_lr):
+        """
+        Do several optimization steps to update the different networks
+        :param step: (int) current timestep
+        :param writer: (TensorboardWriter object)
+        :param current_lr: (float) Current learning rate
+        :return: ([np.ndarray]) values used for monitoring
+        """
+        train_start = time.time()
         mb_infos_vals = []
         for grad_step in range(self.gradient_steps):
             if step < self.batch_size or step < self.learning_starts:
@@ -21,125 +40,136 @@ class SACWithVAE(SAC):
             if (step + grad_step) % self.target_update_interval == 0:
                 # Update target network
                 self.sess.run(self.target_update_op)
+        if self.n_updates > 0:
+            print("SAC training duration: {:.2f}".format(time.time() - train_start))
         return mb_infos_vals
 
     def learn(self, total_timesteps, callback=None, seed=None,
-              log_interval=1, print_freq=100):
-        self._setup_learn(seed)
+              log_interval=1, tb_log_name="SAC", print_freq=100):
 
-        # Transform to callable if needed
-        self.learning_rate = get_schedule_fn(self.learning_rate)
+        with TensorboardWriter(self.graph, self.tensorboard_log, tb_log_name) as writer:
 
-        start_time = time.time()
-        episode_rewards = [0.0]
-        is_teleop_env = hasattr(self.env, "wait_for_teleop_reset")
-        # TeleopEnv
-        if is_teleop_env:
-            print("Waiting for teleop")
-            obs = self.env.wait_for_teleop_reset()
-        else:
-            obs = self.env.reset()
+            self._setup_learn(seed)
 
-        self.episode_reward = np.zeros((1,))
-        ep_info_buf = deque(maxlen=100)
-        ep_len = 0
-        self.n_updates = 0
-        infos_values = []
-        mb_infos_vals = []
-        writer = None
+            # Transform to callable if needed
+            self.learning_rate = get_schedule_fn(self.learning_rate)
 
-        for step in range(total_timesteps):
-            # Compute current learning_rate
-            frac = 1.0 - step / total_timesteps
-            current_lr = self.learning_rate(frac)
-
-            if callback is not None:
-                # Only stop training if return value is False, not when it is None. This is for backwards
-                # compatibility with callbacks that have no return statement.
-                if callback(locals(), globals()) is False:
-                    break
-
-            # Before training starts, randomly sample actions
-            # from a uniform distribution for better exploration.
-            # Afterwards, use the learned policy.
-            if step < self.learning_starts:
-                action = self.env.action_space.sample()
-                # No need to rescale when sampling random action
-                rescaled_action = action
+            start_time = time.time()
+            episode_rewards = [0.0]
+            is_teleop_env = hasattr(self.env, "wait_for_teleop_reset")
+            # TeleopEnv
+            if is_teleop_env:
+                print("Waiting for teleop")
+                obs = self.env.wait_for_teleop_reset()
             else:
-                action = self.policy_tf.step(obs[None], deterministic=False).flatten()
-                # Rescale from [-1, 1] to the correct bounds
-                rescaled_action = action * np.abs(self.action_space.low)
+                obs = self.env.reset()
 
-            assert action.shape == self.env.action_space.shape
+            self.episode_reward = np.zeros((1,))
+            ep_info_buf = deque(maxlen=100)
+            ep_len = 0
+            self.n_updates = 0
+            infos_values = []
+            mb_infos_vals = []
 
-            new_obs, reward, done, info = self.env.step(rescaled_action)
-            ep_len += 1
+            for step in range(total_timesteps):
+                # Compute current learning_rate
+                frac = 1.0 - step / total_timesteps
+                current_lr = self.learning_rate(frac)
 
-            if print_freq > 0 and ep_len % print_freq == 0 and ep_len > 0:
-                print("{} steps".format(ep_len))
+                if callback is not None:
+                    # Only stop training if return value is False, not when it is None. This is for backwards
+                    # compatibility with callbacks that have no return statement.
+                    if callback(locals(), globals()) is False:
+                        break
 
-            # Store transition in the replay buffer.
-            self.replay_buffer.add(obs, action, reward, new_obs, float(done))
-            obs = new_obs
+                # Before training starts, randomly sample actions
+                # from a uniform distribution for better exploration.
+                # Afterwards, use the learned policy.
+                if step < self.learning_starts:
+                    action = self.env.action_space.sample()
+                    # No need to rescale when sampling random action
+                    rescaled_action = action
+                else:
+                    action = self.policy_tf.step(obs[None], deterministic=False).flatten()
+                    # Rescale from [-1, 1] to the correct bounds
+                    rescaled_action = action * np.abs(self.action_space.low)
 
-            # Retrieve reward and episode length if using Monitor wrapper
-            maybe_ep_info = info.get('episode')
-            if maybe_ep_info is not None:
-                ep_info_buf.extend([maybe_ep_info])
+                assert action.shape == self.env.action_space.shape
 
-            if ep_len > self.train_freq:
-                print("Additional training")
-                mb_infos_vals = self.optimize(step, writer, current_lr)
-                self.env.reset()
-                done = True
+                new_obs, reward, done, info = self.env.step(rescaled_action)
+                ep_len += 1
 
-            episode_rewards[-1] += reward
-            if done:
-                if not (isinstance(self.env, VecEnv) or is_teleop_env):
-                    obs = self.env.reset()
+                if print_freq > 0 and ep_len % print_freq == 0 and ep_len > 0:
+                    print("{} steps".format(ep_len))
 
-                print("Episode finished. Reward: {:.2f} {} Steps".format(episode_rewards[-1], ep_len))
-                episode_rewards.append(0.0)
-                ep_len = 0
-                mb_infos_vals = self.optimize(step, writer, current_lr)
+                # Store transition in the replay buffer.
+                self.replay_buffer.add(obs, action, reward, new_obs, float(done))
+                obs = new_obs
 
-                # Refresh obs when using TeleopEnv
-                if is_teleop_env:
-                    print("Waiting for teleop")
-                    obs = self.env.wait_for_teleop_reset()
+                # Retrieve reward and episode length if using Monitor wrapper
+                maybe_ep_info = info.get('episode')
+                if maybe_ep_info is not None:
+                    ep_info_buf.extend([maybe_ep_info])
 
-            # Log losses and entropy, useful for monitor training
-            if len(mb_infos_vals) > 0:
-                infos_values = np.mean(mb_infos_vals, axis=0)
+                if writer is not None:
+                    # Write reward per episode to tensorboard
+                    ep_reward = np.array([reward]).reshape((1, -1))
+                    ep_done = np.array([done]).reshape((1, -1))
+                    self.episode_reward = total_episode_reward_logger(self.episode_reward, ep_reward,
+                                                                      ep_done, writer, step)
 
-            if len(episode_rewards[-101:-1]) == 0:
-                mean_reward = -np.inf
-            else:
-                mean_reward = round(float(np.mean(episode_rewards[-101:-1])), 1)
+                if ep_len > self.train_freq:
+                    print("Additional training")
+                    mb_infos_vals = self.optimize(step, writer, current_lr)
+                    self.env.reset()
+                    done = True
 
-            num_episodes = len(episode_rewards)
-            if self.verbose >= 1 and done and log_interval is not None and len(episode_rewards) % log_interval == 0:
-                fps = int(step / (time.time() - start_time))
-                logger.logkv("episodes", num_episodes)
-                logger.logkv("mean 100 episode reward", mean_reward)
-                logger.logkv('ep_rewmean', safe_mean([ep_info['r'] for ep_info in ep_info_buf]))
-                logger.logkv('eplenmean', safe_mean([ep_info['l'] for ep_info in ep_info_buf]))
-                logger.logkv("n_updates", self.n_updates)
-                logger.logkv("current_lr", current_lr)
-                logger.logkv("fps", fps)
-                logger.logkv('time_elapsed', "{:.2f}".format(time.time() - start_time))
-                if len(infos_values) > 0:
-                    for (name, val) in zip(self.infos_names, infos_values):
-                        logger.logkv(name, val)
-                logger.logkv("total timesteps", step)
-                logger.dumpkvs()
-                # Reset infos:
-                infos_values = []
-        if is_teleop_env:
-            self.env.is_training = False
-        # Use last batch
-        print("Final optimization before saving")
-        self.env.reset()
-        mb_infos_vals = self.optimize(step, writer, current_lr)
+                episode_rewards[-1] += reward
+                if done:
+                    if not (isinstance(self.env, VecEnv) or is_teleop_env):
+                        obs = self.env.reset()
+
+                    print("Episode finished. Reward: {:.2f} {} Steps".format(episode_rewards[-1], ep_len))
+                    episode_rewards.append(0.0)
+                    ep_len = 0
+                    mb_infos_vals = self.optimize(step, writer, current_lr)
+
+                    # Refresh obs when using TeleopEnv
+                    if is_teleop_env:
+                        print("Waiting for teleop")
+                        obs = self.env.wait_for_teleop_reset()
+
+                # Log losses and entropy, useful for monitor training
+                if len(mb_infos_vals) > 0:
+                    infos_values = np.mean(mb_infos_vals, axis=0)
+
+                if len(episode_rewards[-101:-1]) == 0:
+                    mean_reward = -np.inf
+                else:
+                    mean_reward = round(float(np.mean(episode_rewards[-101:-1])), 1)
+
+                num_episodes = len(episode_rewards)
+                if self.verbose >= 1 and done and log_interval is not None and len(episode_rewards) % log_interval == 0:
+                    fps = int(step / (time.time() - start_time))
+                    logger.logkv("episodes", num_episodes)
+                    logger.logkv("mean 100 episode reward", mean_reward)
+                    logger.logkv('ep_rewmean', safe_mean([ep_info['r'] for ep_info in ep_info_buf]))
+                    logger.logkv('eplenmean', safe_mean([ep_info['l'] for ep_info in ep_info_buf]))
+                    logger.logkv("n_updates", self.n_updates)
+                    logger.logkv("current_lr", current_lr)
+                    logger.logkv("fps", fps)
+                    logger.logkv('time_elapsed', "{:.2f}".format(time.time() - start_time))
+                    if len(infos_values) > 0:
+                        for (name, val) in zip(self.infos_names, infos_values):
+                            logger.logkv(name, val)
+                    logger.logkv("total timesteps", step)
+                    logger.dumpkvs()
+                    # Reset infos:
+                    infos_values = []
+            if is_teleop_env:
+                self.env.is_training = False
+            # Use last batch
+            print("Final optimization before saving")
+            self.env.reset()
+            mb_infos_vals = self.optimize(step, writer, current_lr)
         return self
